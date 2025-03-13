d6d9bfe11e886f588d60889ec14233b3abeba718
avindutharushan <avindutharushan2002@gmail.com>
2025-03-13 11:56:51 +0530

    create project with springboot

---
+++ src/test/java/lk/ijse/newslbackend/NewSlBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.newslbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewSlBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
