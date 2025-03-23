32e9abace38565241bc9f83b2317baf8469fa205
avindutharushan <avindutharushan2002@gmail.com>
2025-03-23 11:37:11 +0530

    create enums

---
+++ src/main/java/lk/ijse/newslbackend/entity/enums/IdPrefix.java
@@ -0,0 +1,16 @@
+package lk.ijse.newslbackend.entity.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * This enum class represents the prefixes of the IDs of the entities.
+ * The prefixes of the IDs of the entities can be one of the following:
+ * 1. USR - The prefix of the ID of a user
+ */
+@Getter
+@RequiredArgsConstructor
+public enum IdPrefix {
+    USER("USR");
+    private final String prefix;
+}
